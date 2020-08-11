2ea1bf27961a843243dfcdad3d29ce4ec61ffcb4
murilokrugner <murilokrugner@hotmail.com>
2020-08-11 18:05:47 -0300

    config server

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+import path from 'path';
+
+import routes from './routes';
+
+//import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+  }
+}
+
+export default new App().server;
